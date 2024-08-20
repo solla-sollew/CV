c38d62e00bbb0f73bdb86998c916c93026e35575
Shijun Wang <shijun.james@gmail.com>
2024-08-20 06:56:07 +0000

    A 1st cut at the template

--- flask_app.py
+++ flask_app.py
@@ -1,15 +1,12 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
-
-@app.route('/hello')
-def hello():
-    return 'This is my new shiny Flask app'
+def index():
+    return render_template("main_page.html")
 
