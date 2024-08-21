c0b3ce5340190a76fe2bcb00ec25fb9d2e739892
Shijun Wang <shijun.james@gmail.com>
2024-08-21 09:10:30 +0000

    Added a CV page to the website

--- flask_app.py
+++ flask_app.py
@@ -26,6 +26,10 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
+@app.route("/cv")
+def indexcv():
+    return render_template("index.html")
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
