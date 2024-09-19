a5d73b49936ade5c1839f076f9c9bd6741958211
JamesW <shijun.james@gmail.com>
2024-09-19 11:10:30 +0800

    deploy to azure

--- flask_app.py
+++ flask_app.py
@@ -1,23 +1,10 @@
 
 # A very simple Flask CV with Azure...
 
-from flask import Flask, redirect, render_template, request, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
-
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="",
-    password="",
-    hostname="",
-    databasename="",
-)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 180
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
 
 @app.route("/resume")
 def indexresume():
