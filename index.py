fbfce73930c962e1a881b82536d88313f4c02e8a
JamesW <shijun.james@gmail.com>
2024-10-03 11:38:11 +0800

    Added Credly ICON and update AZ900

--- index.py
+++ index.py
@@ -6,9 +6,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route("/resume")
-def indexresume():
-    return render_template("indexUC.html")
+# @app.route("/resume")
+# def indexresume():
+#     return render_template("indexUC.html")
 
 @app.route("/")
 def indexcv():
